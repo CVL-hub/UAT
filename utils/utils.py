62adac6d60da971b05bdd74d31e71c397752dd16
Computer Vision & Learing Group <167955223+CVL-hub@users.noreply.github.com>
2025-07-08 23:44:38 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,67 @@
+import os
+
+import torch
+import torch.nn.functional as F
+
+
+def set_gpu(gpu_id='0'):
+    if gpu_id == '0':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+        print('USE GPU 0')
+    elif gpu_id == '1':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+        print('USE GPU 1')
+    elif gpu_id == '2':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+        print('USE GPU 2')
+    elif gpu_id == '3':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+        print('USE GPU 3')
+    elif gpu_id == '4':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+        print('USE GPU 4')
+    elif gpu_id == '5':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+        print('USE GPU 5')
+    elif gpu_id == '6':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+        print('USE GPU 6')
+    elif gpu_id == '7':
+        os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+        print('USE GPU 7')
+
+def structure_loss(pred, mask):
+    """
+    loss function (ref: F3Net-AAAI-2020)
+    """
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+    pred = torch.sigmoid(pred)
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1 - (inter + 1) / (union - inter + 1)
+    return (wbce + wiou).mean()
+
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    For calibrating misalignment gradient via cliping gradient technique
+    :param optimizer:
+    :param grad_clip:
+    :return:
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+              
+
+def load_model_params(model, params_path):
+    assert os.path.exists(params_path)
+    checkpoints = torch.load(params_path)
+    # print(checkpoints['epoch'])
+
+    model.load_state_dict(checkpoints['state_dict'])
+    return model
